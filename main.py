82b9956f5f45f6ee30247596666b7581753ec879
최윤준 <choeyunjun@choeyunjun-ui-MacBook-Pro.local>
2018-10-09 17:43:46 +0900

    Loop fix

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 from modules.login import *
+'''
+필요사항
+login.py의        'server_addr': 'http://127.0.0.1:8000' 부분 
 
+
+'''
 
 if __name__ == "__main__":
     if confCheck() == True:
@@ -9,5 +14,6 @@ if __name__ == "__main__":
         loginkey = getLoginKey()
         if loginkey != None:
             if doLogin(loginkey) == True:
-                stdNo, sbjCd, unitCd, pageCnt = getProperty()
-                doAction(stdNo, sbjCd, unitCd, pageCnt)
+                while True:
+                    stdNo, sbjCd, unitCd, pageCnt = getProperty()
+                    doAction(stdNo, sbjCd, unitCd, pageCnt)
